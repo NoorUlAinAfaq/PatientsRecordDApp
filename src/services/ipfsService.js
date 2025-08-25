293abcdb88ad81c3e6add2449240d8af086f8962
Noor Ul Ain Afaq <noorulain.afaqkhatri@gmail.com>
2025-08-25 12:16:22 +0500

    minor update

--- src/services/ipfsService.js
+++ src/services/ipfsService.js
@@ -1,12 +1,15 @@
-// ipfsService.js
 import axios from "axios";
 
+<<<<<<< HEAD
 
 
 const PINATA_GATEWAY = "https://gateway.pinata.cloud/ipfs/";
+=======
+const PINATA_API_KEY = "ff7f3d0b28ecbd4ea94d";
+const PINATA_SECRET_API_KEY = "01e51da7b9f20b48aa7944f2843a038a7da409d6c6a2a1f6542680b8d72351c5";
+>>>>>>> parent of 4a9d083 (removed deactivate from patinetDashboard)
 
-// Upload JSON data to IPFS
-const uploadJSONToIPFS = async (data) => {
+export const uploadToIPFS = async (data) => {
   try {
     const res = await axios.post(
       "https://api.pinata.cloud/pinning/pinJSONToIPFS",
@@ -20,70 +23,17 @@ const uploadJSONToIPFS = async (data) => {
     );
     return res.data.IpfsHash;
   } catch (err) {
-    console.error("Error uploading JSON to Pinata:", err.response?.data || err);
+    console.error("Error uploading to Pinata:", err.response?.data || err);
     throw err;
   }
 };
-
-// Upload file/blob to IPFS
-const uploadFileToIPFS = async (file) => {
-  try {
-    const formData = new FormData();
-    formData.append("file", file);
-
-    const metadata = JSON.stringify({
-      name: file.name || "unnamed_file",
-    });
-    formData.append("pinataMetadata", metadata);
-
-    const options = JSON.stringify({
-      cidVersion: 0,
-    });
-    formData.append("pinataOptions", options);
-
-    const res = await axios.post(
-      "https://api.pinata.cloud/pinning/pinFileToIPFS",
-      formData,
-      {
-        maxBodyLength: "Infinity",
-        headers: {
-          "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
-          pinata_api_key: process.env.PINATA_API_KEY,
-          pinata_secret_api_key: process.env.PINATA_SECRET_API_KEY,
-        },
-      }
-    );
-
-    return res.data.IpfsHash;
-  } catch (err) {
-    console.error("Error uploading file to Pinata:", err.response?.data || err);
-    throw err;
-  }
-};
-
-// Smart wrapper: handles JSON objects OR File/Blob
-export const uploadToIPFS = async (input) => {
-  if (input instanceof File || input instanceof Blob) {
-    return await uploadFileToIPFS(input);
-  } else if (typeof input === "object") {
-    return await uploadJSONToIPFS(input);
-  } else {
-    throw new Error("Unsupported type for IPFS upload");
-  }
-};
-
-// Get data from IPFS
+// Get JSON back from IPFS
 export const getFromIPFS = async (cid) => {
   try {
-    const res = await axios.get(`${PINATA_GATEWAY}${cid}`);
+    const res = await axios.get(`https://gateway.pinata.cloud/ipfs/${cid}`);
     return res.data;
   } catch (err) {
     console.error("Error fetching from Pinata:", err);
     throw err;
   }
-};
-
-// Get file URL from IPFS
-export const getFileURLFromIPFS = (cid) => {
-  return `${PINATA_GATEWAY}${cid}`;
 };
